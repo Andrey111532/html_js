4afad68224ceb3c5e7cddb1b472f78a1f0298d83
Andrey111532 <tsiban73@gmail.com>
2020-03-02 21:00:48 +0200

    shool

--- task_1/script/script.js
+++ task_1/script/script.js
@@ -11,7 +11,56 @@ menu.onclick = function myFunction() {
 }
 
 
-let admin;
-let name = 'Джон';
-admin = name;
-alert(admin);
+
+/*let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!*/
+
+/*let value = prompt("Введите число");
+
+if(value < 0) {
+    value *= -1;
+}
+
+alert(value);*/
+
+/*let month = +prompt("цифра")
+
+switch(month) {
+    case 1: 
+        alert("January");
+        break;
+    case 2: 
+        alert("February")
+        break;
+    case 3: 
+        alert("March")
+        break;
+
+}*/
+/*let age = 3;
+
+for(; age <=7;) {
+    console.log(age++)
+}*/
+
+/*function printHello(name){
+    if (name === undefined){
+        return;
+    }
+    console.log("Hello, " + name );
+}
+printHello("Tom");*/
+
+function hanoi (cir, start, mid, end){
+    if (cir === 1){
+    console.log(start, "->", + end);
+    return;
+    }
+
+    hanoi(cir - 1, start, end, mid);
+    console.log(start, "->", end);
+
+
+   
+
